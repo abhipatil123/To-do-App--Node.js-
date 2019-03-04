d5cbf6ee55b4a6f8be4833738e57042d7c8b5dd7
Patil <amp170830@utdallas.edu>
2019-03-04 11:52:06 -0500

    Storing data to database and rendering view from database

--- controllers/todocontroller.js
+++ controllers/todocontroller.js
@@ -3,15 +3,30 @@ var bodyParser = require('body-parser');
 var data = [{item: 'Do coding'}, {item: 'Update Resume'}, {item: 'Leetode grinding'}];
 var urlencodedParser = bodyParser.urlencoded({extended: false});
 
+var monk = require('monk');
+var db = monk('localhost:27017/TODO');
+
 module.exports = function(app){
     
     app.get('/todo', function(req, res){
-        res.render('todo', {todos : data});
+        //Get data from database and pass it to view
+        var collection = db.get('todos');
+        collection.find({}, function(err, todos){
+            if (err) throw err;
+            res.render('todo', {todos : todos});
+        });
+        // res.render('todo', {todos : data});
     });
 
     app.post('/todo', urlencodedParser, function(req, res){ 
-        data.push(req.body);
-        res.json();
+        var collection = db.get('todos');
+        collection.insert({
+            item: req.body.item,
+        }, function(err, item){
+            if (err) throw err;
+            data.push(item);
+            res.json(item);
+        });
     });
 
     app.delete('/todo/:item', function(req, res){
